71695f1ceaef398c0d870debdafe85bd33bd6383
Francois Polo <andasan@gmail.com>
2020-06-04 10:44:52 -0700

    complete auth formik

--- client/src/user-feature/container/Auth.jsx
+++ client/src/user-feature/container/Auth.jsx
@@ -6,12 +6,25 @@ import CustomTextInput from '../../shared/components/CustomTextInput';
 
 const Auth = () => {
 
+    const validationSchema = Yup.object({
+        username: Yup.string().required('Required'),
+        password: Yup.string().required('Yo, no password?')
+            .matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,' 8 chara, 1 upper, 1 lower, 1 special char')
+    })
+
     return(
         <>
             <Formik
-                initialValues={}
-                validationSchema={}
-                onSubmit={}
+                initialValues={{
+                    username: '',
+                    password: ''
+                }}
+                validationSchema={validationSchema}
+                onSubmit={(value, { setSubmitting, resetForm })=>{
+                    //do api call for login/signup
+                    resetForm();
+                    setSubmitting(false);
+                }}
             >
                 {(props)=>{
                     return(
